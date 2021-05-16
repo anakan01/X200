c5938899d21bda18e1a3b5eb18980a98d691f4ba
Akira <akira.nakano.cbe@gmail.com>
2021-05-16 20:22:41 +0200

    Adjust to match size of bar

--- config.h
+++ config.h
@@ -9,7 +9,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 25;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "Liberation Mono:size=11", "fontawesome:size=11" };
-static const char dmenufont[]       = "Liberation Mono:size=20";
+static const char dmenufont[]       = "Liberation Mono:size=16";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
